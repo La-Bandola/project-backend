e9141105c880ea9616d823022f5d204f0e68d13b
Daniel Orejuela <danielorejuelag@gmail.com>
2026-07-11 21:06:28 -0500

    Escuchar al puerto  3000

--- parcheck_backend/settings.py
+++ parcheck_backend/settings.py
@@ -153,6 +153,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 from datetime import timedelta
