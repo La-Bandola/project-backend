f02f99a90fdb08f288824189be00d11c43d49112
Sara Isabel Sepulveda Perez <sasepulvedap@unal.edu.co>
2026-07-12 23:43:14 -0500

    Ajustes vista parche

--- parcheck_backend/settings.py
+++ parcheck_backend/settings.py
@@ -152,6 +152,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
